f0fe3f6c385aa67c784f627bcbbb910f6c47ecf8
Emmanuel Frecon <efrecon@gmail.com>
2020-09-09 14:30:21 +0200

    Add --auto to ease docker secrets

--- unsecret.sh
+++ unsecret.sh
@@ -5,7 +5,7 @@ set -e
 UNSECRET_VERBOSE=${UNSECRET_VERBOSE:-0}
 UNSECRET_ROOTDIR=${UNSECRET_ROOTDIR:-}
 UNSECRET_ENVSET=${UNSECRET_ENVSET:-}
-
+UNSECRET_AUTO=${UNSECRET_AUTO:-}
 
 # Print usage on stderr and exit
 usage() {
@@ -34,6 +34,9 @@ Usage:
         Root directory under which to locate all files. Default to empty.
     -e | --env(ironment)
         Variable specification, see below. Can be repeated several times.
+    -a | --auto)
+        For all existing variables ending with this suffix, a variable will
+        be created without the suffix and with the content of the file.
 
 Details:
 
@@ -57,9 +60,9 @@ while [ $# -gt 0 ]; do
     -v | --verbose)
         UNSECRET_VERBOSE=1; shift 1;;
 
-    -r | --root-dir | --root | --UNSECRET_ROOTDIR)
+    -r | --root-dir | --root | --rootdir)
         UNSECRET_ROOTDIR=$2; shift 2;;
-    --root-dir=* | --root=* | --UNSECRET_ROOTDIR=*)
+    --root-dir=* | --root=* | --rootdir=*)
         UNSECRET_ROOTDIR="${1#*=}"; shift 1;;
     
     -e | --env | --environment)
@@ -67,6 +70,11 @@ while [ $# -gt 0 ]; do
     --env | --environment)
         UNSECRET_ENVSET=$(printf '%s\n%s' "${1#*=}" "$UNSECRET_ENVSET"); shift 1;;
 
+    -a | --auto )
+        UNSECRET_AUTO=$2; shift 2;;
+    --auto=*)
+        UNSECRET_AUTO="${1#*=}"; shift 1;;
+    
     -h | --help)
         usage; shift 1;;
 
@@ -94,28 +102,46 @@ verbose() {
     if [ "$UNSECRET_VERBOSE" = "1" ]; then printf "%s\n" "$*" 1>&2; fi
 }
 
+setvar() {
+    if [ -z "$2" ]; then
+        _fpath=$(printf %s\\n "$1" | tr '[:upper:]' '[:lower:]' | tr '_' '-')
+    else
+        _fpath=$2
+    fi
+    # Relative path from root dir when root dir is not empty.
+    if [ -n "$UNSECRET_ROOTDIR" ]; then
+        firstchar=$(printf %s\\n "$2" | cut -c1-1)
+        if [ "$firstchar" != "/" ] && [ "$firstchar" != "~" ]; then
+            _fpath="${UNSECRET_ROOTDIR%%/}/${_fpath}"
+        fi
+    fi
+    value=$(cat "$_fpath")
+    verbose "Setting $1 with content of $_fpath"
+    export "${1}=${value}"
+    unset _fpath
+}
+
+if [ -n "$UNSECRET_AUTO" ]; then
+    while read -r setter; do
+        varname=$(printf %s\\n "$setter"|sed -E 's/([^=]+)=(.*)/\1/'|sed "s/${UNSECRET_AUTO}\$//")
+        fpath=$(printf %s\\n "$setter"|sed -E 's/([^=]+)=(.*)/\2/')
+        setvar "$varname" "$fpath"
+    done <<EOC
+$(env | grep -E ".*${UNSECRET_AUTO}=.*")
+EOC
+fi
+
 while read -r cmd; do
     if [ -n "$(printf %s\\n "$cmd" | sed -E 's/^[[:space:]]*$//g')" ]; then
         varname=$(printf %s\\n "$cmd"|awk -F ":" '{print $1}')
         # Path to file is directly specified or comes from name of variable.
         fpath=$(printf %s\\n "$cmd"|awk -F ":" '{print $2}')
-        if [ -z "$fpath" ]; then
-            fpath=$(printf %s\\n "$varname" | tr '[:upper:]' '[:lower:]' | tr '_' '-')
-        fi
-        # Relative path from root dir when root dir is not empty.
-        if [ -n "$UNSECRET_ROOTDIR" ]; then
-            firstchar=$(printf %s\\n "$fpath" | cut -c1-1)
-            if [ "$firstchar" != "/" ] && [ "$firstchar" != "~" ]; then
-                fpath="${UNSECRET_ROOTDIR%%/}/${fpath}"
-            fi
-        fi
-        value=$(cat "$fpath")
-        verbose "Setting $varname with content of $fpath"
-        export "${varname}=${value}"
+        setvar "$varname" "$fpath"
     fi
 done <<EOC
 $UNSECRET_ENVSET
 EOC
 
+# Execute further the rest of the command-line arguments, with environment
+# hopefully properly set.
 [ "$#" -gt "0" ] && exec "$@"
-
